f16e9217175ce31e324896b68f12dde4e8cd9d74
Yurham Afif <yurhamafif.edu@gmail.com>
2022-08-10 09:20:55 +0700

    use testify

--- calc.go
+++ calc.go
@@ -1,10 +1,15 @@
 package main
 
+import "errors"
+
 type Calculator struct {
 	num1 int
 	num2 int
 }
 
-func (c *Calculator) Calculator_Add() int {
-	return c.num1 + c.num2
+func (c *Calculator) Calculator_Add() (int, error) {
+	if c.num1 < 0 || c.num2 < 0 {
+		return -1, errors.New("numbers should never be negative")
+	}
+	return c.num1 + c.num2, nil
 }
--- calc_test.go
+++ calc_test.go
@@ -2,31 +2,54 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-var Test = Calculator{
-	num1: 1,
-	num2: 2,
+// var Test = Calculator{
+// 	num1: 1,
+// 	num2: 2,
+// }
+
+type CalculatorTestSuite struct {
+	suite.Suite
+	calcService Calculator
 }
 
-func TestCalculator_Add(t *testing.T) {
+func (suite *CalculatorTestSuite) SetupTest() {
+	suite.calcService = Calculator{}
+}
+
+// func (suite *CalculatorTestSuite) TestCalculator_Add(t *testing.T) {
+func (suite *CalculatorTestSuite) TestCalculator_Add() {
 	// 1+1=2
-	// t.Logf("hasil:%d", Test.Calculator_Add())
-	// if Test.Calculator_Add() != 3 {
-	// 	t.Errorf("SALAH! harusnya %d", 3)
-	// }
+	// assert.Equal(t, 3, Test.Calculator_Add(), "Seharusnya 3")
 
-	t.Run("", func(t *testing.T) {
-		test := Calculator{
-			num1: 1,
-			num2: 2,
+	testTable := []struct {
+		num1 int
+		num2 int
+		res  int
+	}{
+		{4, 4, 8},
+		{-4, 5, -1},
+	}
+	for _, table := range testTable {
+		suite.calcService.num1 = table.num1
+		suite.calcService.num2 = table.num2
+		actual, err := suite.calcService.Calculator_Add()
+		if err != nil {
+			assert.NotNil(suite.T(), err, "")
+			assert.Equal(suite.T(), table.res, actual, "")
+		} else {
+			assert.Nil(suite.T(), err, "")
+			assert.Equal(suite.T(), table.res, actual, "")
 		}
-		if test.Calculator_Add() != 3 {
-			t.Errorf("SALAH! harusnya %d", 3)
-		}
-	})
+		// expected := 3
+		// assert.Equal(suite.T(), expected, actual, "Seharusnya 3")
+	}
 }
 
-func TestMain(t *testing.T) {
-	main()
+func TestCalculatorTestSuite(t *testing.T) {
+	suite.Run(t, new(CalculatorTestSuite))
 }
